60bb002fe15bb70c8c84fd7fc55c4a657c7ea980
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-07-14 09:03:04 +0200

    castor_windy: add device specific overlay
    and rename common overlay to avoid confusion
    
    Change-Id: Ie81986491e71b30fd493ce8e4dd8760a307b4219

--- castor-common.mk
+++ castor-common.mk
@@ -15,7 +15,7 @@
 # Inherit the fusion-common definitions
 $(call inherit-product, device/sony/shinano-common/shinano.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/sony/castor_windy/overlay
+DEVICE_PACKAGE_OVERLAYS += device/sony/castor_windy/overlay-common
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
