dc56639e65f43f3461e46ef6333ee3a5c3fe0e01
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-12 16:42:59 +0530

    Update memory configuration inheritance
    * Use 'inherit-product' instead of 'inherit-product-if-exists'
      for the Dalvik heap config, and HWUI memory limits,
      so that the build fails if those files do not exist
    * Device doesn't boot with the default values, it
      runs Out-of-memory because of improper configuration
    
    Change-Id: I90f02a118f3791e482bbe49507256f5e2e7cc47a

--- castor-common.mk
+++ castor-common.mk
@@ -36,7 +36,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc
 
 # call dalvik heap config
-$(call inherit-product-if-exists, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
 # call hwui memory config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
