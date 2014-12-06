5ce738c6210713a677c975d31ff7b024e5c79b78
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-12-06 16:22:59 +0100

    update signature
    Change-Id: I318ee6d37f88e5718933d3b0f4b9de92b8604d90

--- cm.mk
+++ cm.mk
@@ -6,7 +6,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/castor_windy/full_castor_windy.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP511 BUILD_FINGERPRINT=Sony/SGP511/SGP511:4.4.2/17.1.2.A.0.314/xf5vdw:user/release-keys PRIVATE_BUILD_DESC="SGP511-user 4.4.2 17.1.2.A.0.314 xf5vdw release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP511
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP511/SGP511:4.4.4/23.0.1.A.0.167/WP_3Rw:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP511-user 4.4.4 23.0.1.A.0.167 WP_3Rw release-keys"
 
 PRODUCT_NAME := cm_castor_windy
 PRODUCT_DEVICE := castor_windy
