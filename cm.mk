da232f53eb657c58cc119c61fa1ab72480da6b62
David Viteri <davidteri91@gmail.com>
2015-08-04 20:34:34 +0200

    castor_windy: update fingerprint to latest lollipop release
    Change-Id: I5505721564a6bad118380411e26f689cb4e93976
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- cm.mk
+++ cm.mk
@@ -7,8 +7,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, device/sony/castor_windy/full_castor_windy.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP511
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP511/SGP511:5.0.2/23.1.A.0.690/2466713625:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP511-user 5.0.2 23.1.A.0.690 2466713625 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP511/SGP511:5.1.1/23.4.A.0.546/3701983898:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP511-user 5.1.1 23.4.A.0.546 3701983898 release-keys"
 
 PRODUCT_NAME := cm_castor_windy
 PRODUCT_DEVICE := castor_windy
