619e1812b621baa5a7f7dded9622ae5ab9bea4a8
Fábio Silva <fabio.fernando.osilva@gmail.com>
2016-02-27 12:01:43 -0300

    castor_windy: move touchscreen *.idc to device tree
    Change-Id: Ic64cb7bea6c8299fd6df1a26fe27bef6aa175fd0

--- castor-common.mk
+++ castor-common.mk
@@ -44,6 +44,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
 
+# Touchscreen
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/clearpad.idc:system/usr/idc/clearpad.idc
+
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
