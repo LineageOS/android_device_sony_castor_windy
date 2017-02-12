8b636e838e14bbb9fbd19e852d67b3f947ecc6de
Max Weffers <rcstar6696@gmail.com>
2017-02-12 09:48:34 +0100

    Remove deprecated enhanced nfc

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,6 @@
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/sony/castor_windy/full_castor_windy.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP511
