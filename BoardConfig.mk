0487a5ab2f24d03ced8acf3b44f7acaccb5929f8
Max Weffers <rcstar6696@gmail.com>
2017-03-10 11:18:25 +0100

    Use new defconfig for castor_windy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,4 +20,4 @@ TARGET_OTA_ASSERT_DEVICE := SGP511,SGP512,castor_windy
 TARGET_BOOTLOADER_BOARD_NAME := SGP511,SGP512
 
 # Kernel properties
-TARGET_KERNEL_CONFIG := cm_shinano_castor_windy_defconfig
+TARGET_KERNEL_CONFIG := lineageos_shinano_castor_windy_defconfig
