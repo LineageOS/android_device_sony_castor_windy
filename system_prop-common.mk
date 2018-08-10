6511d9b55ebca6450108c6623792112c21d7de60
Bruno Martins <bgcngm@gmail.com>
2018-08-10 23:14:57 +0100

    castor_windy: Import stock Dalvik heap overrides
    Change-Id: Ifac6650a874cf982a6e767da92599d4b6230a637

--- system_prop-common.mk
+++ system_prop-common.mk
@@ -1,3 +1,12 @@
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
+
 # LCD
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240
