fe9f5d084bdb7cf7f83788fdefab603e6d2114ed
Georg Veichtlbauer <georg@vware.at>
2020-12-12 16:44:00 +0100

    castor_windy: remove cmhw class
    * now handled in shinano-common
    
    Change-Id: I2e2e37013ca850be061d890ff040a20038cf6cd9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -17,8 +17,6 @@ include device/sony/shinano-common/BoardConfigCommon.mk
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/castor_windy/include
 
-BOARD_HARDWARE_CLASS += device/sony/castor_windy/cmhw
-
 # For two-stage boot configuration
 TARGET_RECOVERY_DEVICE_DIRS := device/sony/castor_windy
 
